e4a5c14e6601b15b7cfe4808cb6c211f51b48655
Nayara.Fugii <nayarac.neto@gmail.com>
2020-11-16 15:42:20 -0300

    inclusao de função com o .map

--- src/data.js
+++ src/data.js
@@ -1,9 +1,11 @@
 // estas funciones son de ejemplo
+import data from "./data/pokemon/pokemon.js"
 
-export const example = () => {
-  return 'example';
-};
+const orderName = data.pokemon.map(item => console.log(item.name))
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+
+
+
+// export const anotherExample = () => {
+//   return 'OMG';
+// };
