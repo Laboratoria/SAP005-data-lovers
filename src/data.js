f583494b2f1cd51903efc8d951f3bfd51f1969ab
Nayara.Fugii <nayarac.neto@gmail.com>
2020-11-16 18:03:00 -0300

    inclusao da função de filtro por nome e ordem alfabetica com fotos

--- src/data.js
+++ src/data.js
@@ -1,11 +1,44 @@
-// estas funciones son de ejemplo
+
 import data from "./data/pokemon/pokemon.js"
 
-const orderName = data.pokemon.map(item => console.log(item.name))
+const filter = {
+  photo(){
+    const image = data.pokemon.map(img =>{
+      return {
+        name: img.name,
+        img: img.img
+      }
+    })
+    console.log(image)
+  },
+
+  filter(){
+    const orderName = data.pokemon.map(item => {
+      
+      return {
+        name: item.name,
+        img: item.img
+      }
+    })
+    const order = orderName.sort(function (pokemon1, pokemon2){
+      if (pokemon1.name > pokemon2.name) {
+            return 1;
+          }
+          if (pokemon1.name < pokemon2.name) {
+            return -1;
+          }
+         
+          return 0;
+    });    
+    
+  },
+}
+
+
+
+filter.photo();
 
 
 
 
-// export const anotherExample = () => {
-//   return 'OMG';
-// };
+
