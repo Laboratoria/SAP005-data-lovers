307f8387a18e6378e34c8f4087d7b95374b5c767
gabriella-guimaraes <gabigara13@gmail.com>
2020-11-17 17:34:30 -0300

    inicio da manipulação de arrays/objetos

--- src/data.js
+++ src/data.js
@@ -1,8 +1,6 @@
 // estas funciones son de ejemplo
+//import data from './data/rickandmorty/rickandmorty.js';
 
-export const example = () => {
-  return 'example';
-};
 
 export const anotherExample = () => {
   return 'OMG';
