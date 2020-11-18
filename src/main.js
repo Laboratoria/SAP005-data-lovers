bd4e2d2ae2b999f5d0db08b467bb25ce06fb8eb7
Nayara.Fugii <nayarac.neto@gmail.com>
2020-11-18 20:42:28 -0300

    inclusao dos filtros na tela

--- src/main.js
+++ src/main.js
@@ -15,6 +15,73 @@ function showAllPokemons() {
     });
 }
 
+const nameOrder = filter.filter()
+const nameFilter = document.querySelector('.orderName');
+let select = "";
+
+nameFilter.addEventListener('change',(event)=>{
+    select = event.target.value;
+    console.log(select)
+    document.getElementById('cards').innerHTML= "";
+
+    if ("" === event.target.value) {
+        showAllPokemons()
+    } else if(select === 'az'){
+        nameOrder.forEach(pokemon => {
+            const image = document.createElement("img");
+            image.src = pokemon.img
+            document.getElementById('cards').appendChild(image);
+            // const text = document.createElement('p');
+            // text.innerHTML = pokemon.name   
+            // document.getElementById('cards').appendChild(text); 
+    
+        });
+
+    }else{
+        let newArray = filter.filter()
+        newArray.reverse()
+        newArray.forEach(pokemon => {
+            const image = document.createElement("img");
+            image.src = pokemon.img
+            document.getElementById('cards').appendChild(image);
+            // const text = document.createElement('p');
+            // text.innerHTML = pokemon.name   
+            // document.getElementById('cards').appendChild(text); 
+    
+        });
+
+    }
+    
+})
+
+
+const typeElement = filter.type()
+const selectType = document.querySelector('.filterType');
+let choiseType = "";
+
+selectType.addEventListener('change', (event) => {
+     choiseType = event.target.value;
+     console.log(choiseType)
+    document.getElementById('cards').innerHTML = "";
+
+    if ("" === event.target.value) {
+        showAllPokemons()
+    } else {
+        typeElement.forEach(pokemonType => {
+            let typeList = pokemonType.type;
+
+            typeList.forEach(pokemon => {
+                if (pokemon === choiseType) {
+                    const image = document.createElement("img");
+                    image.src = pokemonType.img
+                    document.getElementById('cards').appendChild(image);
+                }
+            });
+        });
+    }
+})
+
+
 
 const weaknesses = filter.weaknesses()
 const selectElement = document.querySelector('.fraquezas');
@@ -39,4 +106,4 @@ selectElement.addEventListener('change', (event) => {
             });
         });
     }
-});
+})
