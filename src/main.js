68f3ff44ad4393c802334a97db16474138adb7d5
Carina Rocha <criasdacaz@gmail.com>
2020-11-18 14:26:56 -0300

    alteracao na animacao e insercao de elementos no main

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
-import { example } from './data.js';
+const hamburguer =document.querySelector(".hamburguer");
+
+hamburguer.addEventListener("click", function(){
+    document.querySelector(".container").classList.toggle("show-menu");
+})
+
+
+
+//import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+//import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+//console.log(example, data);
