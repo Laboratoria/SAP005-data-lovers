72c0afc6d4be9fb3f0cbbb77eaf5e94db3d8d08d
gabriella-guimaraes <gabigara13@gmail.com>
2020-11-18 20:57:48 -0300

    criação de uma função para exibir dados dos personagems

--- src/main.js
+++ src/main.js
@@ -3,16 +3,41 @@
 //import data from './data/pokemon/pokemon.js';
 import data from './data/rickandmorty/rickandmorty.js';
 //import charactersInfo from './data.js';
-// show all characters on HTML
-const allCharacters = data.results
-for(let allCharacter of allCharacters){
-    console.log(allCharacter.name); 
-    const showAllCharactersName = document.getElementById("showAllCharactersName");
-    showAllCharactersName.innerHTML = allCharacter.name;
-}    
+//show all characters (basic) info on HTML
 //show filter result: gender (import the array from data.js)
 //show filter result: status dead/alive characters (import the array from data.js)
 //show filter result: names (alphabetic order, ascending order, descending order) (import the array from data.js)
 //show filter result: kind (import the array from data.js)
 //show filter result: place of origin (import the array from data.js)
 //console.log(example, data);
+for(let result of data.results){
+    showInfo(result);
+}
+function showInfo(cards){
+    let showCharactersInfo = document.getElementById("cards");
+    let charactersList = document.createElement("li");
+    for(let [key,value] of Object.entries(cards)){
+        if (key === "name"){
+            let p = document.createElement("p");
+            p.appendChild(document.createTextNode(value));
+            charactersList.appendChild(p);
+            p.id = "name";
+        } else if (key === "status"){
+            let p = document.createElement("p")
+            p.appendChild(document.createTextNode("Status: " + value));
+            charactersList.appendChild(p);
+            p.id = "status"
+        } else if (key === "species"){
+            let p = document.createElement("p");
+            p.appendChild(document.createTextNode(value));
+            charactersList.appendChild(p);
+            p.id = "species";
+        } else if (key === "gender"){
+            let p = document.createElement("p");
+            p.appendChild(document.createTextNode(value));
+            charactersList.appendChild(p);
+            p.id = "gender"
+        }
+    }
+    showCharactersInfo.appendChild(charactersList);
+}
