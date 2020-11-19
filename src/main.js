a0fc6ad38c8f1f222de120eb48bb8f26169bb667
Juliana <juliana.souzas@yahoo.com.br>
2020-11-19 16:53:59 -0300

    docs(style page): adicao do filtro de busca

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+//import data from './data/lol/lol.js';
+//import data from './data/pokemon/pokemon.js';
+ import data from './data/rickandmorty/rickandmorty.js';
+
+const especiesFilter = document.getElementById("especie");
+
+
+
+
+
+
 
 console.log(example, data);
