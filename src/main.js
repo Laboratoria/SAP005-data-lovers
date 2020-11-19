02c8cc5fb01179424056ca17cd461aa365324188
karinefrontelli <karinefrontellidossantos@gmail.com>
2020-11-18 21:36:27 -0300

    Inclusão da funçao força

--- src/main.js
+++ src/main.js
@@ -17,14 +17,14 @@ function showAllPokemons() {
 
 
 const weaknesses = filter.weaknesses()
-const selectElement = document.querySelector('.fraquezas');
+const selectElementWeaknesses = document.querySelector('.weaknesses');
 let elementoSelecionado = "";
 
-selectElement.addEventListener('change', (event) => {
+selectElementWeaknesses.addEventListener('change', (event) => {
      elementoSelecionado = event.target.value;
     document.getElementById('cards').innerHTML = "";
 
-    if ("" === event.target.value) {
+    if ("" === elementoSelecionado) {
         showAllPokemons()
     } else {
         weaknesses.forEach(pokemon => {
@@ -32,6 +32,31 @@ selectElement.addEventListener('change', (event) => {
 
             weaknessesList.forEach(weaknessesItem => {
                 if (weaknessesItem === elementoSelecionado) {
+                    const image = document.createElement("img");
+                    image.src = pokemon.img
+                    document.getElementById('cards').appendChild(image);
+                }
+            });
+        });
+    }
+});
+
+const resistant = filter.resistant()
+const selectElementResistant = document.querySelector('.resistant');
+let elementoSelecionadoResistant = "";
+
+selectElementResistant.addEventListener('change', (event) => {
+    elementoSelecionadoResistant = event.target.value;
+    document.getElementById('cards').innerHTML = "";
+
+    if ("" === elementoSelecionadoResistant) {
+        showAllPokemons()
+    } else {
+        resistant.forEach(pokemon => {
+            let resistantList = pokemon.resistant;
+
+            resistantList.forEach(resistantItem => {
+                if (resistantItem === elementoSelecionadoResistant) {
                     const image = document.createElement("img");
                     image.src = pokemon.img
                     document.getElementById('cards').appendChild(image);
