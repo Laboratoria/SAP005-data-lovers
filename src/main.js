5788bebe784fdce4d9ec84121bb68dfe24fcd596
JulianaAmoriN <juamorim.sn@gmail.com>
2020-11-20 18:28:54 -0300

    link com o pokemon.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
-const hamburguer =document.querySelector(".hamburguer");
+import pokemon from './data/pokemon/pokemon.js';
+import data from './data/pokemon/pokemon.js';
 
+const hamburguer =document.querySelector(".hamburguer");
 hamburguer.addEventListener("click", function(){
-    document.querySelector(".container").classList.toggle("show-menu");
+    document.getElementById("root").classList.toggle("show-menu");
 })
 
-
-
+for(let card of data.pokemon){
+    console.log(card.img)
+    console.log(card.name)
+    console.log(card.about)
+}
 //import { example } from './data.js';
 // import data from './data/lol/lol.js';
 //import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-//console.log(example, data);
+//console.log(data.pokemon[0].name);
