c1fd84539c155c239b909ce2735b05914396cabe
Julia Terin <jucterin@gmail.com>
2020-11-21 02:53:51 -0300

    html e css do layout

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
-import { example } from './data.js';
+//import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+//import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+//console.log(example, data);
+const fila = document.querySelector('.container-carrossel');
+const pokemon = document.querySelector('.pokemon');
+
+const flechaEsquerda = document.getElementById('flecha-esquerda');
+const flechaDireita = document.getElementById('flecha-direita');
+
+//event listener para a flecha direita
+
+flechaDireita.addEventListener('click', () => {
+    
+})
