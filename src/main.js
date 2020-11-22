62f4fef1bd75b8adf4cd090a2b897e7e199c10e5
Talita-8 <tlthmf1@outlook.com>
2020-11-21 23:12:28 -0300

    cards/Novo layout

--- src/main.js
+++ src/main.js
@@ -6,24 +6,19 @@ console.log(example, data);
 
 /* -------------MENU E BOTÕES DESKTOP VERSION------------ */ 
 
-// const charactersMenu = document.getElementById("characters-options")
 const charactersButton = document.getElementById("characters-button")
-// const charactersCard = document.getElementById("card-info")
 charactersButton.addEventListener("click", showCharacters)
 
-/* Aproveitei sua função! Cards são gerados no click de Personagens.
-    function showCharacters(){
-    charactersMenu.style.visibility = "visible"
-    charactersCard.style.visibility = "visible"
-}
-*/
+
 
 function showCharacters(){
 	for (let character of data.results){
-		let cards = document.getElementById("card-info");
-		let newCard = document.createElement("div");
+		let cards = document.getElementById("get-cards");
+        let newCard = document.createElement("div");
 		cards.appendChild(newCard);
-        newCard.innerHTML = `<div class="card-front"><img class="front-pic" src="${character.image}" alt=""><h1>${character.name}</h1></div>
+        newCard.innerHTML = `<div class="all-cards">
+        <div class="card-info">
+         <div class="card-front"><img class="front-pic" src="${character.image}" alt=""><h1>${character.name}</h1></div>
             <div class="card-back">
             <img class="back-pic" src="${character.image}" alt="">
             <ul>
@@ -35,20 +30,41 @@ function showCharacters(){
             <li>Origin: ${character.origin.name}</li>
             <li>Location: ${character.location.name}</li>
             </ul>
-            </div>`;
+            </div>
+          </div>  
+        </div>`;
 	}
 }
 
 
 /* --------------MENU E BOTÕES MOBILE VERSION--------------- */
 
-const characMenuMobile = document.getElementById("characters-options-mobile")
-const characButtonMobile = document.getElementById("button-charac")
-const characCardMobile = document.getElementById("card-info-mobile")
-characButtonMobile.addEventListener("click", showCharacMobile)
+const characMobiButton = document.getElementById("button-charac-mobile")
+characMobiButton.addEventListener("click", showMobiCharac)
 
 
-function showCharacMobile(){
-    characMenuMobile.style.visibility = "visible"
-    characCardMobile.style.visibility = "visible"
+
+function showMobiCharac(){
+	for (let character of data.results){
+		let cardsMobi = document.getElementById("get-cards-mobile");
+        let newCardMobi = document.createElement("div");
+		cardsMobi.appendChild(newCardMobi);
+        newCardMobi.innerHTML = `<div class="all-cards-mobile">
+        <div class="card-info-mobile">
+         <div class="card-front-mobile"><img class="front-pic-mobile" src="${character.image}" alt=""><h1>${character.name}</h1></div>
+            <div class="card-back-mobile">
+            <img class="back-pic-mobile" src="${character.image}" alt="">
+            <ul>
+            <li>Name: ${character.name}</li>
+            <li>Status: ${character.status}</li>
+            <li>Species: ${character.species}</li> 
+            <li>Type: ${character.type}</li>
+            <li>Gender: ${character.gender}</li> 
+            <li>Origin: ${character.origin.name}</li>
+            <li>Location: ${character.location.name}</li>
+            </ul>
+            </div>
+          </div>  
+        </div>`;
+	}
 }
