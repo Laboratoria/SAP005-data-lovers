9fee3c58e1b0d61bbde4c815cd11ec6fef81e77b
JulianaAmoriN <juamorim.sn@gmail.com>
2020-11-23 14:07:57 -0300

    imprimi lista de pokemons na html

--- src/main.js
+++ src/main.js
@@ -1,16 +1,28 @@
-import pokemon from './data/pokemon/pokemon.js';
 import data from './data/pokemon/pokemon.js';
 
-const hamburguer =document.getElementById("menu-hamburguer");
+const hamburguer = document.getElementById("menu-hamburguer");
 hamburguer.addEventListener("click", () => {
-    document.getElementById("root").classList.toggle("show-menu");
+  document.getElementById("root").classList.toggle("show-menu");
 })
 
-for(let card of data.pokemon){
-    console.log(card.img)
-    console.log(card.name)
-    console.log(card.about)
+function pokebola() {
+  for (let card of data.pokemon) {
+    document.getElementById("pokedex").innerHTML += `
+    <li class="card-poke">
+        <img class="card-image" src="${card.img}" alt="${card.img}/>
+        <h2 class="card-title">${card.num}. ${card.generation.name}</h2>
+        <p class="card-subtitle">Type: ${card.type.toString().replace(","," ")}</p>
+    </li>`
+  }
 }
+
+pokebola(data.pokemon);
+
+//const ul = document.getElementById("pokedex")
+//ul.innerHTML = pokebola.bola(data)
+
+
+
 //import { example } from './data.js';
 // import data from './data/lol/lol.js';
 //import data from './data/pokemon/pokemon.js';
