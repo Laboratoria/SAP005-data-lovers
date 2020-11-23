85e7c72106e3d0acb6e31fd4f62fa78306063e54
anny_@DESKTOP-ACQR4TU <martins.stfn@gmail.com>
2020-11-23 18:50:35 -0300

    Estilo Cards CSS e Pagina History and Curiosities

--- src/main.js
+++ src/main.js
@@ -12,40 +12,82 @@ showingCards(data.pokemon)
 
 function showingCards(pokemonCards) {
 
-    let showCards = document.querySelector('#main-cards')
-    let cards = ""
-    showCards.innerHTML = ""
+    let showCards = document.querySelector('#main-cards');
+    let cards = "";
+    showCards.innerHTML = "";
 
     for (let pokemon of pokemonCards) {
         cards +=
 
-            `<div class="frame-left">
-                <div class="left">
-                <div class = "card-front"><img src = ${pokemon.img} class = "img" alt = ${pokemon.name}/> 
-                    <p> N°: ${pokemon.num}</p> 
-                    <p> Nome: ${pokemon.name} </p> 
+            `<div class="frame left">
+                
+                <div class="left"><h3 class = "card-front"><img src = ${pokemon.img} class = "img" alt = ${pokemon.name}</h3> 
+                    <p> N°: ${pokemon.num}</p> <p> Nome: ${pokemon.name} </p> 
                 </div>
                 <div class="card-back"> 
-                    <p> Type: ${pokemon.type}</p>
-                    <p> Height: ${pokemon.size.height}
-                    <p> Weight: ${pokemon.size.weight}</p>
-                    <p> Resistant: ${pokemon.resistant}</p>
-                    <p> Weaknesses: ${pokemon.weaknesses}</p>
-                    <p> Base Attack: ${pokemon.stats["base-attack"]}</p>
-                    <p> Base Defense: ${pokemon.stats["base-defense"]}</p>
-                    <p> Base Stamina: ${pokemon.stats["base-stamina"]}</p>
-                    <p> Max Cp: ${pokemon.stats["max-cp"]}</p>
-                    <p> Max Hp: ${pokemon.stats["max-hp"]}</p>
-                    <p> Rarity: ${pokemon["pokemon-rarity"]}</p>
-                    <p> Region: ${pokemon.generation.name}</p>
+                    <h6> Type: ${pokemon.type}</h6>
+                    <h6> Height: ${pokemon.size.height}</h6>
+                    <h6> Weight: ${pokemon.size.weight}</h6>
+                    <h6> Resistant: ${pokemon.resistant}</h6>
+                    <h6> Weaknesses: ${pokemon.weaknesses}</h6>
+                    <h6> Base Attack: ${pokemon.stats["base-attack"]}</h6>
+                    <h6> Base Defense: ${pokemon.stats["base-defense"]}</h6>
+                    <h6> Base Stamina: ${pokemon.stats["base-stamina"]}</h6>
+                    <h6> Max Cp: ${pokemon.stats["max-cp"]}</h6>
+                    <h6> Max Hp: ${pokemon.stats["max-hp"]}</h6>
+                    <h6> Rarity: ${pokemon["pokemon-rarity"]}</h6>
+                    <h6> Region: ${pokemon.generation.name}</h6>
                     </div>
             </div>`
+
+
     }
     showCards.innerHTML = cards;
+
 }
 
 inputSearch.addEventListener('keyup', () => {
     const searchName = inputSearch.value
     const pokemons = instantSearch(searchName, data.pokemon)
     showingCards(pokemons)
+
 })
+
+
+// ordenar de a-z
+
+
+// var id = 0;
+// var j = "";
+// var p = 151;
+
+// newSort();
+// function newSort() {
+//     for (id; id < p; id++) {
+//     var orderPoke = data.pokemon[id].name;
+//     orderPoke.sort;
+//     console.log(orderPoke);
+//     }
+    
+// }
+
+
+// selectSort.addEventListener('sort', () => {
+//     let ordenar = selectSort.value
+//     let pokemons = sort(ordenar, data.pokemon)
+
+
+// } 
+
+
+// )
+    // var selectSort = document.querySelector('#select-sort')
+    // selectSort.value = '';
+    // ordenar = .sort();
+
+    // for(let i = 0; i < pokemon.lenght; i++){
+    //    return Ordenar = showingCards(pokemon.name)
+    // }
+
+
+
