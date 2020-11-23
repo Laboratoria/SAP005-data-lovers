8d339ea7e44a79d2c668c7bc9a09317323ec76e1
Beatriz Penalva <biapenalva@gmail.com>
2020-11-23 13:45:31 -0300

    coments only

--- src/data.js
+++ src/data.js
@@ -1,9 +1,8 @@
-// estas funciones son de ejemplo
-
-export const example = () => {
+/*export const example = () => {
   return 'example';
 };
 
 export const anotherExample = () => {
   return 'OMG';
 };
+*/
