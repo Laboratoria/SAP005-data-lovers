06518863b87d7d6f9cf1cd265100220db766d618
gabriella-guimaraes <gabigara13@gmail.com>
2020-11-24 20:48:38 -0300

    Adição de funções para filtragem de dados

--- src/data.js
+++ src/data.js
@@ -1,7 +1,16 @@
 // estas funciones son de ejemplo
-//import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/rickandmorty/rickandmorty.js';
+export const elements = data.results
 
-
-export const anotherExample = () => {
-  return 'OMG';
+export const info = (vector) => {
+  return vector.map(function (item){
+    return [item.name, item.gender, item.gender, item.status, item.species]
+  })
 };
+export const orderAZ = (item) => {
+  return info(item).sort();
+};
+export const orderZA = (item) => {
+  return orderAZ(item).reverse();
+}
+
