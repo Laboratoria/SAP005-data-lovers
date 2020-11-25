e9469e580f3e8f80c5493acaec1fccd516da49df
Tânagra Andria <design.andria@gmail.com>
2020-11-25 14:27:46 -0300

    Ajustes

--- src/data.js
+++ src/data.js
@@ -1,16 +1,1 @@
-// export const applySearchType = ( pokemon, filterType) => pokemon. filter ((search)=> (search.type.includes (filterType)));
-// export  const  resultCalcType  =  ( pokemon,  filterType )  =>
-//   ( ( filterType ) / ( pokemon ) * 100 ) ;
 
-// export  const  applySearchName  =  ( pokemon ,  filterName )  =>  pokemon . filtro ( ( search )  =>
-//   ( search . name . toLowerCase ( ) . indexOf (filterName . toLowerCase ( ) )  >  - 1 ) ) ;
-
-//   export  const  resultCalcType  =  ( pokemon,  filterType )  =>
-//   ( ( filterType ) / ( pokemon ) * 100 ) ;
-
-//   return 'example';
-// };
-
-/*export const anotherExample = () => {
-  return 'OMG';
-};*/
