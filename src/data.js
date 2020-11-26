c39e83f30e2167a734c0849000f5e73293e0d918
akemimeka <akemimeka@gmail.com>
2020-11-26 19:07:45 -0300

    Exemplos de como exibir dados para o usuário

--- src/data.js
+++ src/data.js
@@ -1,9 +1,17 @@
-// estas funciones son de ejemplo
+export const filterRivalPokemon = (pokemons, getSearchInput) => {
+  for (const pokemon of pokemons) {
+    if (pokemon.name == getSearchInput.value) {
+      return {
+        "number": pokemon.num,
+        "name": pokemon.name,
+        "image": pokemon.img,
+        "type": pokemon.type
+      };
+    }
+  }
+}
+// export const filterBestPokemonByType = pokemons => {
+//   for (const pokemon of pokemons) {
 
-export const example = () => {
-  return 'example';
-};
-
-export const anotherExample = () => {
-  return 'OMG';
-};
+//   }
+// }
--- src/main.js
+++ src/main.js
@@ -1,27 +1,25 @@
-// import { example } from './data.js';
 // import pokemon from './data/pokemon/pokemon.js';
+import { filterRivalPokemon } from './data.js';
 import data from './data/pokemon/pokemon.js';
 
+// LEMBRETE: a variável 'pokemons' é um ARRAY!
 const pokemons = data.pokemon;
 const searchButton = document.getElementById("search-btn");
 const getSearchInput = document.getElementById("search-input");
 const pokemonTable = document.getElementById("pokemon-table");
+const pokemonName = document.getElementById("pokemon-name")
+const pokemonType = document.getElementById("pokemon-type")
+const pokemonNumber = document.getElementById("pokemon-num")
 
-searchButton.addEventListener("click", SearchPokemon);
-
-function SearchPokemon(event) {
+// BUSCA DO POKÉMON RIVAL
+searchButton.addEventListener("click", event => {
   event.preventDefault();
-  for (const pokemon of pokemons) {
-    if (pokemon.name == getSearchInput.value) {
-      return pokemon;
-    }
-  }
-}
+  const rivalPokemon = filterRivalPokemon(pokemons, getSearchInput)
 
+  // EXIBIÇÃO DO NOME E TIPO DO POKÉMON RIVAL
+  pokemonNumber.innerHTML = `Número: ${rivalPokemon.number}`
+  pokemonName.innerHTML = `Nome: <span class="first-uppercase">${rivalPokemon.name}</span>`
+  pokemonType.innerHTML = `Tipo(s): ${rivalPokemon.type}`
+});
 
-
-// LEMBRETE: a variável pokemons é um ARRAY!
 // TABELA
-
-
-
