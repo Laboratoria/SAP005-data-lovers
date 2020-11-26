8877b31c4d8dc3a28db30cde7553a6a29c8d1305
akemimeka <akemimeka@gmail.com>
2020-11-25 22:46:06 -0300

    Adiciona HTML e JS básico de busca do pokémon no main.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,27 @@
-import { example } from './data.js';
+// import { example } from './data.js';
+// import pokemon from './data/pokemon/pokemon.js';
 import data from './data/pokemon/pokemon.js';
 
+const pokemons = data.pokemon;
 const searchButton = document.getElementById("search-btn");
+const getSearchInput = document.getElementById("search-input");
+const pokemonTable = document.getElementById("pokemon-table");
 
 searchButton.addEventListener("click", SearchPokemon);
 
 function SearchPokemon(event) {
   event.preventDefault();
-  
-  const getSearchInput = document.getElementById("search-input").value;
-  
+  for (const pokemon of pokemons) {
+    if (pokemon.name == getSearchInput.value) {
+      return pokemon;
+    }
+  }
 }
+
+
+
+// LEMBRETE: a variável pokemons é um ARRAY!
+// TABELA
+
+
+
