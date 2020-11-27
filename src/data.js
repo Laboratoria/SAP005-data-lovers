bdc5c79bc18511f17704559c03cd6a9ef1222c19
Jessica Cavalcante <je-cavalcante@hotmail.com>
2020-11-27 15:09:09 -0300

    create function order

--- src/data.js
+++ src/data.js
@@ -4,19 +4,22 @@ function getPokemonIndexFromUrl() {
   const urlParameters = new URLSearchParams(queryString);
 
   return urlParameters.get('pokeId');
-}; 
-function filterData(data,name) {
-  return data.filter(pokemon => pokemon.name.includes(name));
-}
+};
+
 function filterData(data,condition) {
   return data.filter(pokemon => { 
     if(condition.name != 'undefined' && pokemon.name.includes(condition.name)) {
       return true;
     }
-    if(condition.type &&  pokemon.type.indexOf(condition.type) != -1) {
+    if(condition.type &&  pokemon.type.indexOf(condition.type) != -1 || condition.type == "") {
       return true;
     }
     return false;
   });
 };
-export {getPokemonIndexFromUrl,filterData,};
+
+function orderData(data) {
+  data.sort();
+}
+
+export {getPokemonIndexFromUrl, filterData, orderData};
--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import data from './data/pokemon/pokemon.js';
-import {filterData} from './data.js';
+import {filterData, orderData} from './data.js';
 
 const search = document.getElementById('filter-name');
 search.addEventListener('keyup', function(){
   let pokemons= filterData(data.pokemon, {name: search.value.toLowerCase()} );
   document.getElementById('root').innerHTML = "";
+  load(pokemons);
+})
+
+const order = document.getElementById('select-option');
+order.addEventListener('change', function (){
+  let pokemons= filterData(data.pokemon, {type: order.options[order.selectedIndex].value});
+  document.getElementById('root').innerHTML = "";
+  orderData(pokemons);
   load(pokemons);
 })
 
