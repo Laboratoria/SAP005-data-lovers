8a0cb342e70087bb2ff9fd84b43c95b5fe34d111
caroline costa <carolmarcionilia@gmail.com>
2020-11-28 12:08:04 -0300

    corrige funções e adiciona logo

--- src/main.js
+++ src/main.js
@@ -8,9 +8,9 @@ const selectFilterWeaknesses = document.querySelector('#select-filter-by-weaknes
 const selectFilterGeneration = document.querySelector('#select-filter-by-generation')
 const selectSortCp = document.querySelector('#select-sort-cp')
 const selectSort = document.querySelector('#select-sort')
-const btnClean = document.querySelector('#btn-clean')
 
 showingCards(data.pokemon)
+    //showingCards(data.pokemon.sort((poke1, poke2) => poke1.name.localeCompare(poke2.name)))
 
 function showingCards(pokemonCards) {
     let showCards = document.querySelector('#main-cards')
@@ -19,7 +19,7 @@ function showingCards(pokemonCards) {
 
     for (let pokemon of pokemonCards) {
         let evolutions = ""
-        if (pokemon.evolution["next-evolution"] != undefined && pokemon.evolution["next-evolution"] != null) {
+        if (!!pokemon.evolution["next-evolution"]) {
             for (let evolution of pokemon.evolution["next-evolution"]) {
                 evolutions += `<span>${evolution.name}</span>`
             }
@@ -31,23 +31,23 @@ function showingCards(pokemonCards) {
             `<div class="frame left">
                 <div class="left">
                 <h3 class = "card-front"><img src = ${pokemon.img} class = "img" alt = ${pokemon.name}</h3> 
-                    <p>N°:${pokemon.num}</p>
-                    <p>Nome:${pokemon.name}</p>
-                    <p>Type:${pokemon.type}</p>
-                    <p>Rarity:${pokemon["pokemon-rarity"]}</p>                    
+                    <p>N°: ${pokemon.num}</p>
+                    <p>Nome: ${pokemon.name}</p>
+                    <p>Type: ${pokemon.type}</p>
+                    <p>Rarity: ${pokemon["pokemon-rarity"]}</p>
+                    <p>Evolution: ${evolutions}<p>                 
                 </div>
                 <div class="card-back">
-                    <p>Height:${pokemon.size.height}</p>
+                    <p>Height: ${pokemon.size.height}</p>
                     <p>Weight: ${pokemon.size.weight}</p>
-                    <p>Resistant:${pokemon.resistant}</p>
-                    <p>Weaknesses:${pokemon.weaknesses}</p>
-                    <p>Base Attack:${pokemon.stats["base-attack"]}</p>
-                    <p>Base Defense:${pokemon.stats["base-defense"]}</p>
-                    <p>Base Stamina:${pokemon.stats["base-stamina"]}</p>
-                    <p>Max Cp:${pokemon.stats["max-cp"]}</p>
-                    <p>Max Hp:${pokemon.stats["max-hp"]}</p>
-                    <p>Evolution:${evolutions}<p>
-                    </div>
+                    <p>Resistant: ${pokemon.resistant}</p>
+                    <p>Weaknesses: ${pokemon.weaknesses}</p>
+                    <p>Base Attack: ${pokemon.stats["base-attack"]}</p>
+                    <p>Base Defense: ${pokemon.stats["base-defense"]}</p>
+                    <p>Base Stamina: ${pokemon.stats["base-stamina"]}</p>
+                    <p>Max Cp: ${pokemon.stats["max-cp"]}</p>
+                    <p>Max Hp: ${pokemon.stats["max-hp"]}</p>
+                </div>
             </div>`
     }
     showCards.innerHTML = cards;
@@ -57,7 +57,9 @@ function showingCards(pokemonCards) {
 inputSearch.addEventListener('keyup', () => {
     const searchName = inputSearch.value
     const pokemons = instantSearch(searchName, data.pokemon)
-    showingCards(pokemons)
+    const selectType = selectFilterType.value
+    const pokemonType = selectPokemonType(selectType, pokemons)
+    showingCards(pokemonType)
 
 })
 
@@ -84,3 +86,14 @@ selectFilterGeneration.addEventListener('change', () => {
     const pokemonGeneration = selectPokemonGeneration(selectGeneration, data.pokemon)
     showingCards(pokemonGeneration)
 })
+
+
+data.pokemon.sort((a, b) => {
+    const keyA = a.name
+    const keyB = b.name;
+    if (keyA < keyB) return -1;
+    if (keyA > keyB) return 1;
+    return 0;
+
+});
+console.log(data.pokemon);
