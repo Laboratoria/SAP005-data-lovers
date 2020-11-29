1eb2420a85901f2d59df103c30d7a08d9eb0d3ff
akemimeka <akemimeka@gmail.com>
2020-11-29 00:52:38 -0300

    Aprimora função para filtrar rival, cria função que retorna array com fraquezas do rival e cria função que retorna tipos dos pokémons

--- src/data.js
+++ src/data.js
@@ -1,17 +1,67 @@
-export const filterRivalPokemon = (pokemons, getSearchInput) => {
+import data from "./data/pokemon/pokemon.js"
+
+const pokemons = data.pokemon;
+
+export const filterRivalPokemon = (pokemons, userInput) => {
   for (const pokemon of pokemons) {
-    if (pokemon.name == getSearchInput.value) {
+    const lowercaseUserInput = String(userInput.value).toLowerCase();
+    const acceptingUserInput = pokemon.name.includes(lowercaseUserInput);
+
+    if (acceptingUserInput) {
       return {
         "number": pokemon.num,
         "name": pokemon.name,
         "image": pokemon.img,
-        "type": pokemon.type
-      };
+        "type": pokemon.type,
+        "weakness": pokemon.weaknesses,
+        "attack": pokemon.stats["base-attack"],
+        "defense": pokemon.stats["base-defense"],
+        "stamina": pokemon.stats["base-stamina"],
+        "cp": pokemon.stats["max-cp"],
+        "hp": pokemon.stats["max-hp"],
+      }
     }
   }
-}
-// export const filterBestPokemonByType = pokemons => {
-//   for (const pokemon of pokemons) {
+};
 
+const rivalWeaknessArray = (pokemons, userInput) => {
+  const rivalPokemon = filterRivalPokemon(pokemons, userInput);
+
+  return rivalPokemon.weakness;
+}
+
+const pokemonTypeArray = () => {
+  for (const pokemon of pokemons) {
+    return pokemon.type
+  }
+}
+
+const filterBestChoicesByType = () => {
+  rivalWeaknessArray.filter(type => {
+    pokemonTypeArray.includes(type)
+  })
+}
+
+// export const filterBestChoicesByType = (pokemons, userInput) => {
+//   const rivalPokemon = filterRivalPokemon(pokemons, userInput)
+//   // const rivalWeakness = () => {
+//   //   for (const weakness of rivalPokemon.weaknesses) {
+//   //     return weakness
+//   //   }
+//   // }
+
+//   const pokemonType = () => {
+//     for (const pokemon in pokemons) {
+//       return pokemon.type
+//     }
 //   }
+
+//   for (const weakness in rivalPokemon.weaknesses) {
+//     for (const type in pokemonType) {
+//       if (weakness === type) {
+//         return pokemon
+//       }
+//     }
+//   }
+
 // }
--- src/main.js
+++ src/main.js
@@ -1,25 +1,23 @@
-// import pokemon from './data/pokemon/pokemon.js';
-import { filterRivalPokemon } from './data.js';
 import data from './data/pokemon/pokemon.js';
+import { filterRivalPokemon } from './data.js';
 
 // LEMBRETE: a variável 'pokemons' é um ARRAY!
 const pokemons = data.pokemon;
+console.log(pokemons)
 const searchButton = document.getElementById("search-btn");
 const getSearchInput = document.getElementById("search-input");
 const pokemonTable = document.getElementById("pokemon-table");
-const pokemonName = document.getElementById("pokemon-name")
-const pokemonType = document.getElementById("pokemon-type")
-const pokemonNumber = document.getElementById("pokemon-num")
 
 // BUSCA DO POKÉMON RIVAL
 searchButton.addEventListener("click", event => {
   event.preventDefault();
   const rivalPokemon = filterRivalPokemon(pokemons, getSearchInput)
 
-  // EXIBIÇÃO DO NOME E TIPO DO POKÉMON RIVAL
-  pokemonNumber.innerHTML = `Número: ${rivalPokemon.number}`
-  pokemonName.innerHTML = `Nome: <span class="first-uppercase">${rivalPokemon.name}</span>`
-  pokemonType.innerHTML = `Tipo(s): ${rivalPokemon.type}`
+  // TABELA
+  pokemonTable.innerHTML = `
+    Nome: ${rivalPokemon.name} <br>
+    Tipo: ${rivalPokemon.type}
+  `
+
 });
 
-// TABELA
