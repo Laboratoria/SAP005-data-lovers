7f34247c70b0e83e35db37942712607958f8cd85
akemimeka <akemimeka@gmail.com>
2020-11-29 18:40:04 -0300

    Cria função com filtro para separar pokémons por tipo

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ import data from "./data/pokemon/pokemon.js"
 
 const pokemons = data.pokemon;
 
-export const filterRivalPokemon = (pokemons, userInput) => {
+export function filterRivalPokemon(pokemons, userInput) {
   for (const pokemon of pokemons) {
     const lowercaseUserInput = String(userInput.value).toLowerCase();
     const acceptingUserInput = pokemon.name.includes(lowercaseUserInput);
@@ -11,10 +11,80 @@ export const filterRivalPokemon = (pokemons, userInput) => {
       return pokemon
     }
   }
-};
-
-export const rivalWeaknessArray = (pokemons, userInput) => {
-  const rivalPokemon = filterRivalPokemon(pokemons, userInput);
-
-  return rivalPokemon.weakness;
 }
+
+export function filterPokemonByType(pokemons) {
+  let bugType = []; let dragonType = []; let fairyType = [];
+  let fireType = []; let ghostType = []; let groundType = [];
+  let normalType = []; let psychicType = []; let steelType = [];
+  let darkType = []; let electricType = []; let fightingType = [];
+  let flyingType = []; let grassType = []; let iceType = [];
+  let poisonType = []; let rockType = []; let waterType = [];
+
+  for (const pokemon of pokemons) {
+    const pokemonType = pokemon.type;
+    for (const type of pokemonType) {
+      switch (type) {
+        case 'bug':
+          bugType.push(pokemon)
+          break;
+        case 'dragon':
+          dragonType.push(pokemon)
+          break;
+        case 'fairy':
+          fairyType.push(pokemon)
+          break;
+        case 'fire':
+          fireType.push(pokemon)
+          break;
+        case 'ghost':
+          ghostType.push(pokemon)
+          break;
+        case 'ground':
+          groundType.push(pokemon)
+          break;
+        case 'normal':
+          normalType.push(pokemon)
+          break;
+        case 'psychic':
+          psychicType.push(pokemon)
+          break;
+        case 'steel':
+          steelType.push(pokemon)
+          break;
+        case 'dark':
+          darkType.push(pokemon)
+          break;
+        case 'electric':
+          electricType.push(pokemon)
+          break;
+        case 'fighting':
+          fightingType.push(pokemon)
+          break;
+        case 'flying':
+          flyingType.push(pokemon)
+          break;
+        case 'grass':
+          grassType.push(pokemon)
+          break;
+        case 'ice':
+          iceType.push(pokemon)
+          break;
+        case 'poison':
+          poisonType.push(pokemon)
+          break;
+        case 'rock':
+          rockType.push(pokemon)
+          break;
+        case 'water':
+          waterType.push(pokemon)
+          break;
+      }
+    }
+  }
+  return {
+    bugType, dragonType, fairyType, fireType, ghostType, groundType, normalType, psychicType, steelType, darkType, electricType, fightingType, flyingType, grassType, iceType, poisonType, rockType, waterType
+  }
+}
+
+
--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import data from './data/pokemon/pokemon.js';
-import { filterRivalPokemon } from './data.js';
+import { filterRivalPokemon, filterPokemonByType } from './data.js';
 
 // LEMBRETE: a variável 'pokemons' é um ARRAY!
 const pokemons = data.pokemon;
-console.log(pokemons)
+// console.log(pokemons)
 const searchButton = document.getElementById("search-btn");
 const getSearchInput = document.getElementById("search-input");
 const pokemonTable = document.getElementById("pokemon-table");
@@ -11,16 +11,18 @@ const pokemonTable = document.getElementById("pokemon-table");
 
 // BUSCA DO POKÉMON RIVAL
 searchButton.addEventListener("click", event => {
-  event.preventDefault();
   const rivalPokemon = filterRivalPokemon(pokemons, getSearchInput)
+  event.preventDefault();
 
   // CARD DO POKÉMON RIVAL
+
+
+  // TABELA DE POKÉMONS VANTAJOSOS
   pokemonTable.innerHTML = `
     Nome: ${rivalPokemon.name} <br>
     Fraqueza: ${rivalPokemon.weaknesses}
   `
 
-  // TABELA DE POKÉMONS VANTAJOSOS
-  
+  const rivalWeaknesses = rivalPokemon.weaknesses;
 
 });
