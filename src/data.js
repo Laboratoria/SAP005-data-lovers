64b46cc58dc35c887a4c092f807ee97612ce6019
RoSapia <roberta.sapia@hotmail.com>
2020-11-29 16:47:31 -0300

    add página pokedex

--- src/data.js
+++ src/data.js
@@ -1,14 +1,14 @@
+/*filterData(data, condition): esta função receberia os dados e nos retornaria os que cumprem com a condição.
+sortData(data, sortBy, sortOrder): esta função recebe três parâmetros. O primeiro, data, nos entrega os dados. O segundo, sortBy, diz respeito a qual das informações quer usar para ordenar. O terceiro, sortOrder, indica se quer ordenar de maneira crescente ou decrescente.
+computeStats(data): essa função nos permite fazer cálculos estatísticos básicos para serem exibidos de acordo com o que os dados permitem.
+*/
 const pokemon = {
   pesquisaPokemon(data, nomePokemon) {
-    console.log(nomePokemon)
    let listaPokemon = data.pokemon
    console.log(listaPokemon)
-   let busca = listaPokemon.find(i => i.name == nomePokemon)
-   console.log(busca)
+   let busca = listaPokemon.find(p => p.name == nomePokemon)
    return (busca)
-
  }
-
 }
 
 export default pokemon
--- src/main.js
+++ src/main.js
@@ -1,31 +1,31 @@
-import pokemon  from './data.js';
+import pokemon from './data.js';
 
 import data from './data/pokemon/pokemon.js';
 
 const pesquisa = document.getElementById("pesquisar")
-pesquisa.addEventListener("click", function(evt){
+pesquisa.addEventListener("click", function (evt) {
     evt.preventDefault()
     const textoPesquisa = document.getElementById("pesquisa-pokemon").value
     console.log(textoPesquisa)
     let resultado = pokemon.pesquisaPokemon(data, textoPesquisa.toString().toLowerCase())
-const localNumero = document.getElementById("numero-pokemon")
-localNumero.innerHTML = resultado.num
-const imagemPokemon = document.getElementById("imagem-pokemon")
-imagemPokemon.src = resultado.img
-const nomePokemon = document.getElementById("nome-pokemon")
-nomePokemon.innerHTML = resultado.name
-document.getElementById("altura").innerHTML = "Height:"
-const alturaPokemon = document.getElementById("altura-pokemon")
-alturaPokemon.innerHTML = resultado.size.height
-document.getElementById("peso").innerHTML = "Weight:"
-const pesoPokemon = document.getElementById("peso-pokemon")
-pesoPokemon.innerHTML = resultado.size.weight
-document.getElementById("tipo").innerHTML = "Type:"
-const tipoPokemon = document.getElementById("tipo-pokemon")
-tipoPokemon.innerHTML = resultado.type
-document.getElementById("fraqueza").innerHTML = "Weaknesses:"
-const fraquezaPokemon = document.getElementById("fraqueza-pokemon")
-fraquezaPokemon.innerHTML = resultado.weaknesses
+    const localNumero = document.getElementById("numero-pokemon")
+    localNumero.innerHTML = resultado.num
+    const imagemPokemon = document.getElementById("imagem-pokemon")
+    imagemPokemon.src = resultado.img
+    const nomePokemon = document.getElementById("nome-pokemon")
+    nomePokemon.innerHTML = resultado.name
+    document.getElementById("altura").innerHTML = "Height:"
+    const alturaPokemon = document.getElementById("altura-pokemon")
+    alturaPokemon.innerHTML = resultado.size.height
+    document.getElementById("peso").innerHTML = "Weight:"
+    const pesoPokemon = document.getElementById("peso-pokemon")
+    pesoPokemon.innerHTML = resultado.size.weight
+    document.getElementById("tipo").innerHTML = "Type:"
+    const tipoPokemon = document.getElementById("tipo-pokemon")
+    tipoPokemon.innerHTML = resultado.type
+    document.getElementById("fraqueza").innerHTML = "Weaknesses:"
+    const fraquezaPokemon = document.getElementById("fraqueza-pokemon")
+    fraquezaPokemon.innerHTML = resultado.weaknesses
 })
 
 
