64a8918766ce2c4d619a8234eea87905469af079
cbalieiro <bmoliveira.camila@gmail.com>
2020-11-30 22:48:42 -0300

    JS/HTML - Ajustes para o run teste

--- src/data.js
+++ src/data.js
@@ -1,23 +1,13 @@
-// estas funciones son de ejemplo
-
-export const searchFind = (array, valueTxt) => {
-  let retornoArray = array.find((item) =>  item.num === valueTxt.toLowerCase());
-  return retornoArray
-};
+export const searchFind = (array, valueTxt) => 
+array.find(item => item.num === valueTxt.toLowerCase());
 
 export const crescent = (array) => {
-  return array.sort(function(a, b) {
-    return a.name > b.name ? 1 : -1;
-  });
-};
+  array.sort(function (a, b) { return a.name > b.name ? 1 : -1; })
+}
 
 export const decrescent = (array) => {
-  return array.sort(function(a, b) {
-    return a.name < b.name ? 1 : -1;
-  });
-};
+  array.sort(function (a, b) { return a.name < b.name ? 1 : -1; })
+}
 
 export const searchFilter = (array, valueTxt) => {
-let retornoArray = array.filter((item) =>  item.type.includes(valueTxt));
-return retornoArray
-};
+array.filter((item) => item.type.includes(valueTxt))}
