62cf2b765a8570457101ca2f7790d15a2fb68d93
aluizada <luizanunes.b@gmail.com>
2020-12-01 19:51:06 -0300

    botao-logica

--- src/main.js
+++ src/main.js
@@ -1,41 +1,42 @@
-import pokemons from '../data/pokemon/pokemon.json';
+import pokemons from '../data/pokemon/pokemon.js';
 import { getType, getName, getEvolution } from './data.js';
 
-// function mostrarCards()
+// function cadaitem
 
 
-console.log("oi")
+
+console.log(pokemons)
     //Mostrar todos os pokémons//
 
-document.getElementById("types-menu").addEventListener('change', (e) => x(e.target.value))
+// document.getElementById("types-menu").addEventListener('click', (e) => x(e.target.value, console.log(e)))
+document.getElementById("types-menu").addEventListener('click', (e) => window.prompt("funcionou"))
+    // document.getElementsByName("pokemon-name")[0].addEventListener('change', (e) => buscarPorNome(e.target.value))
 
-document.getElementsByName("pokemon-name")[0].addEventListener('change', (e) => buscarPorNome(e.target.value))
-
-document.getElementById("evolucao").addEventListener('click', evolution)
+// document.getElementById("evolucao").addEventListener('click', evolution)
 
 
-const filtrarTipo = (event) => {
-    let typeArray = getType(pokemons, event);
-    renderPokemon(typeArray)
-}
+// const filtrarTipo = (event) => {
+//     let typeArray = getType(pokemons, event);
+//     renderPokemon(typeArray)
+// }
 
-document.getElementById("types-menu").addEventListener('change', (e) => filtrarTipo(e.target.value))
+// document.getElementById("types-menu").addEventListener('change', (e) => filtrarTipo(e.target.value))
 
-//Buscar por nome//
-const buscarPorNome = (event) => {
-    let typeArray = getName(pokemons, event);
-    renderPokemon(typeArray)
-}
+// //Buscar por nome//
+// const buscarPorNome = (event) => {
+//     let typeArray = getName(pokemons, event);
+//     renderPokemon(typeArray)
+// }
 
-document.getElementsByName("pokemon-name")[0].addEventListener('change', (e) => buscarPorNome(e.target.value))
+// document.getElementsByName("pokemon-name")[0].addEventListener('change', (e) => buscarPorNome(e.target.value))
 
-//Evolucao//
+// //Evolucao//
 
-function evolution() {
-    let name = document.getElementById("poke-name").value;
-    let pokemonEvolution = getEvolution(pokemons, name);
-    renderPokemon(pokemonEvolution)
+// function evolution() {
+//     let name = document.getElementById("poke-name").value;
+//     let pokemonEvolution = getEvolution(pokemons, name);
+//     renderPokemon(pokemonEvolution)
 
-}
+// }
 
-document.getElementById("evolucao").addEventListener('click', evolution)
+// document.getElementById("evolucao").addEventListener('click', evolution)
