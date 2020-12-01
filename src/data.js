525355ef006bf6b354b064bffb74ffa01ddccb93
cbalieiro <bmoliveira.camila@gmail.com>
2020-12-01 14:23:46 -0300

    JS/refatorando

--- src/data.js
+++ src/data.js
@@ -1,14 +1,23 @@
-export const searchFind = (array, valueTxt) => 
-array.find(item => item.num === valueTxt.toLowerCase());
-
+export const searchFind = (array, valueTxt) => {
+  return array.find(item => item.num === valueTxt.toLowerCase())
+}
 
 export const crescent = (array) => {
-  array.sort(function (a, b) { return a.name > b.name ? 1 : -1; })
+  return array.sort(function (a, b) { return a.name > b.name ? 1 : -1; })
 }
 
 export const decrescent = (array) => {
-  array.sort(function (a, b) { return a.name < b.name ? 1 : -1; })
+  return array.sort(function (a, b) { return a.name < b.name ? 1 : -1; })
 }
 
 export const searchFilter = (array, valueTxt) => {
-array.filter((item) => item.type.includes(valueTxt))}
+  return array.filter((item) => item.type.includes(valueTxt))
+}
+
+export const example = () => {
+  return 'example';
+};
+
+export const anotherExample = () => {
+  return 'OMG';
+};
