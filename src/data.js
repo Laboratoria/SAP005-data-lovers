53470191999f28e5f581afe4132dd52a4678da24
unknown <saraazabel@gmail.com>
2020-12-02 20:22:17 -0300

    select

--- src/data.js
+++ src/data.js
@@ -1,11 +1,17 @@
 const nomePersonagem = {
   pesquisaPersonagem(data, personagem){
-    let dadosPersonagem = data.results
-    console.log(dadosPersonagem)
-    let busca = dadosPersonagem.find(p => p.name == personagem)
-    console.log(busca)
-    return (busca) 
-    
+    const pegarDados = data.results
+    const pegarNome = personagem.pegarDados.name
+
+    console.log(pegarNome)
+
+
+    // const dadosPersonagem = data.results
+    // console.log(dadosPersonagem)
+    // let busca = dadosPersonagem.find(p => p.name == personagem)
+    // console.log(busca)
+    // return (busca)
+
   }
-} 
+}
 export default nomePersonagem
--- src/main.js
+++ src/main.js
@@ -1,12 +1,23 @@
 import nomePersonagem from './data.js';
 import data from './data/rickandmorty/rickandmorty.js';
 //DOM, nós, registro de manejadores de eventos (event listeners ou event handlers).
-const personagens = document.getElementById("botaoPersonagem")
-personagens.addEventListener("click", function(evt){
-  evt.preventDefault()
-  const pesquisaPersonagem = document.getElementById("personagens").value
-  let resultado = nomePersonagem.pesquisaPersonagem(data, pesquisaPersonagem.toString())
-  document.getElementById("card1").innerHTML = resultado.species
 
-})
+function listarPersonagens (lista){
+  let option = ""
+  for (let personagem of lista){
+    option += `<option>${personagem.name}</option>`
+  }
+  return option
+}
+const opcoesPersonagens = document.getElementById("personagens")
+opcoesPersonagens.innerHTML = listarPersonagens(data.results)
+
+// const personagens = document.getElementById("botaoPersonagens")
+// personagens.addEventListener("click", function(evt){
+//   evt.preventDefault()
+//   const pesquisaPersonagem = document.getElementById("personagens").value
+//   let resultado = nomePersonagem.pesquisaPersonagem(data, pesquisaPersonagem.toString())
+//   document.getElementById("card1").innerHTML = resultado.species
+
+// })
 
