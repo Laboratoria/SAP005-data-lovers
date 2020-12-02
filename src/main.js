a7df5743e166abe0ebcebd74400a6e4ab96a2ede
akemimeka <akemimeka@gmail.com>
2020-12-02 11:13:51 -0300

    Insere estrutura para tabela no HTML e loop para pokémons serem exibidos em cada row da tabela em main.js

--- src/main.js
+++ src/main.js
@@ -3,22 +3,44 @@ import { filterRivalPokemon, orderBestPokemonByCP } from './data.js';
 
 const pokemons = data.pokemon;
 const searchButton = document.getElementById("search-btn");
-const pokemonTable = document.getElementById("pokemon-table");
+const pokemonTableRow = document.querySelector(".pokemon-table-row");
 
 
 // BUSCA DO POKÉMON RIVAL
 searchButton.addEventListener("click", event => {
   event.preventDefault();
-  const rivalPokemon = filterRivalPokemon(pokemons)
+  // const rivalPokemon = filterRivalPokemon(pokemons)
+  const best30PokemonByDecrescentCP = orderBestPokemonByCP().decrescentOrder.slice(0, 40)
 
   // CARD DO POKÉMON RIVAL
 
 
+
   // TABELA DE POKÉMONS VANTAJOSOS
-  pokemonTable.innerHTML = `
-    Nome: ${rivalPokemon.name} <br>
-    Fraqueza: ${rivalPokemon.weaknesses}
-  `
+
+  // dentro do LOOP
+  function showTable(best30PokemonByDecrescentCP) {
+    let row = ""
+    for (let pokemon of best30PokemonByDecrescentCP) {
+      row += `
+        <tr>
+          <td>${pokemon.num}</td>
+          <td>${pokemon.name}
+            <img class="pokemon-img" src="${pokemon.img}">
+          </td>
+          <td>${pokemon.type}</td>
+          <td>${pokemon.resistant}</td>
+          <td>${pokemon.stats["max-cp"]}</td>
+          <td>${pokemon.stats["base-attack"]}</td>
+          <td>${pokemon.stats["base-defense"]}</td>
+        </tr>
+      `
+    }
+    return row
+  }
+
+
+  pokemonTableRow.innerHTML = showTable(best30PokemonByDecrescentCP)
 
   // console.log(orderBestPokemonByCP())
 })
