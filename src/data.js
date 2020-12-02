2628fbdbc58a4884ba3afd23648f2300e11467cf
RoSapia <roberta.sapia@hotmail.com>
2020-12-02 10:36:31 -0300

    add funcionalidade mostrar pokemons na tela

--- src/data.js
+++ src/data.js
@@ -2,13 +2,19 @@
 sortData(data, sortBy, sortOrder): esta função recebe três parâmetros. O primeiro, data, nos entrega os dados. O segundo, sortBy, diz respeito a qual das informações quer usar para ordenar. O terceiro, sortOrder, indica se quer ordenar de maneira crescente ou decrescente.
 computeStats(data): essa função nos permite fazer cálculos estatísticos básicos para serem exibidos de acordo com o que os dados permitem.
 */
+
 const pokemon = {
   pesquisaPokemon(data, nomePokemon) {
-   let listaPokemon = data.pokemon
-   console.log(listaPokemon)
-   let busca = listaPokemon.find(p => p.name == nomePokemon)
-   return (busca)
- }
+    const listaPokemon = data.pokemon
+    let busca = listaPokemon.filter(p => p.name == nomePokemon)
+    return (busca)
+  },
+  filtrarLendarios(data) {
+    const listaPokemon = data.pokemon
+    const lendarios = listaPokemon.filter(p => p['pokemon-rarity'] === 'legendary')
+    console.log(lendarios)
+    return (lendarios)
+  }
 }
 
 export default pokemon
