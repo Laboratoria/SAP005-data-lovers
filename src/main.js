eda44107aea57446314d27196ea8b6bd5d51f905
alencartha <alencar_tha@hotmail.com>
2020-12-03 17:37:38 -0300

    flip do card unitário

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { crescent, decrescent, searchFilter, searchFind } from './data.js';
 import data from './data/pokemon/pokemon.js';
 const copyDB = data.pokemon;
 
+
 // Limpa a DIV que irá receber todos os CARDS (Todos, Filtrados ou Ordenados) 
 function clearCards() {
     document.getElementById("item-all-cards").innerHTML = ""
@@ -10,15 +11,36 @@ function clearCards() {
 /*                            Funções para Templates de Cards                            */
 function templateOneCard(array) {
     let pokecard = "";
-    pokecard += `<div id="item-card1">
-<img src="${array.img}">
-<h1>${array.name.toUpperCase()}<h1>
-<p>${array.num}</p> 
-<p>${array.about}</p> 
-<p> <strong>Tipo:</strong> ${array.type.join(' / ')}</p>
-<p> <strong>Resistente:</strong> ${array.resistant.join(' / ')}</p></div>`
-    return document.getElementById("item-card-one").innerHTML = pokecard
+    pokecard += 
+            `<div id="item-card1">
+                <img src="${array.img}">
+                <h1>${array.name.toUpperCase()}<h1>
+                <p>${array.num}</p> 
+                <p>${array.about}</p> 
+                <p> <strong>Tipo:</strong> ${array.type.join(' / ')}</p>
+                <p> <strong>Resistente:</strong> ${array.resistant.join(' / ')}</p>
+            </div>`
+            
+
+    return document.getElementById("item-card-one-front").innerHTML = pokecard
 }
+
+function templateOneCardBack(array) {
+    let pokecardback = "";
+    pokecardback += 
+            `<div id="item-card-back">
+                <p><strong> Height:</strong> ${array.size.height}</p>
+                <p><strong> Weight:</strong> ${array.size.weight}</p>
+                <p> <strong>Generation:</strong> ${array.generation.num}</p>
+                <p> <strong>Weaknesses:</strong> ${array.weaknesses.join(' / ')}</p>
+                <p><strong> Candy- Evolution:</strong> ${array.evolution.candy}</p>  
+                <p> <strong>Egg:</strong> ${array.egg}</p> 
+            </div>`
+            
+
+    return document.getElementById("item-card-one-back").innerHTML = pokecardback
+}
+
 
 
 function templateAllCards(array) {
@@ -38,11 +60,21 @@ function templateAllCards(array) {
 
 /*                            Todas as funções ---> Na sequência > CardUnitário> TodosOsCards> OrdenaCrescent > OrdenaDecrescente > FiltraporTipo                          */
 function cardOne() {
-    document.getElementById("item-card-one").innerHTML = "";
+    document.getElementById("item-card-one-front").innerHTML = "";
     let txtSearch = document.getElementById('txtBusca').value;
     const resultSearch = searchFind(copyDB, txtSearch);
     return templateOneCard(resultSearch)
 }
+
+function cardOneBack() {
+    document.getElementById("item-card-one-back").innerHTML = "";
+    let txtSearch = document.getElementById('txtBusca').value;
+    const resultSearch = searchFind(copyDB, txtSearch);
+    return templateOneCardBack(resultSearch)
+}
+
+
+
 
 // Botão e Event para enter localizados na section class="middle-bg" do html.
 document.getElementById('btn-search-pokemon').addEventListener("click", cardOne);
@@ -54,6 +86,17 @@ input.addEventListener("keyup", function (event) {
         cardOne()
     }
 })
+
+document.getElementById('btn-search-pokemon').addEventListener("click", cardOneBack);
+let input2 = document.getElementById("txtBusca");
+input2.addEventListener("keyup", function (event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        cardOneBack()
+    }
+})
+
+
 
 //Responsável por carregar em Cards todo o Objeto Data / Array Pokemon.
 function cardAll() {
