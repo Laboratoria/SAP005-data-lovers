7f564463c4de0be04a4cbfef046700413cbf0e47
Julia Terin <jucterin@gmail.com>
2020-12-04 15:19:06 -0300

    filtro funcionndo

--- src/data.js
+++ src/data.js
@@ -1,47 +1,26 @@
-/*export const example = () => {
-  return 'example';
+export const searchByName = (name, data) => {
+  return data.filter((select) => select.name.includes(name.toLowerCase()));
 };
 
-export const anotherExample = () => {
+export const selectType = (selectByType, data) => {
+  return data.filter((select) => select.type.includes(selectByType));
+};
 
-  return 'OMG';
-};*/
+export const orderBy = (orderBy, data) => {
+  if (orderBy === 'order-az') {
+    data.sort(function(a, b) {
+      var textA = a.name.toLowerCase();
+      var textB = b.name.toLowerCase();
+      return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+    })
+  }
 
-//Primeira tentativa
-
-
-/*const pokemons = data.pokemon.slice(0, 102); //arrays
-export const pokemons;
-
-const buttonName = document.getElementById('btname')
-buttonName.addEventListener('click', functionName)
-
-function functionName(clickBtName){ 
-  clickBtName.preventDefault();
-  let pokeNome = document.getElementById('text').value;
-  pokeNome = pokeNome.toUpperCase();
-  let resultName = pokemons.map(function (pokemons) {
-  return pokemons.name
-  })
-}
-
-
-const buttonTipo = document.getElementById('pokeTipos')
-buttonTipo.addEventListener('click', functionType)  
-
-function functionType(clickType){
-  clickType.preventDefault(); 
-  let select = document.getElementById('pokeTipos');
-  let option = select.options[select.selectedIndex];
-  document.getElementById('tipos').value = option.tipos;
-  let typeWater = pokemons.filter(pokemons => {
-    pokemons.type.includes("water")
-    return typeWater
-  )}*/
-
-  export const selectType = (selectByType, data) =>{
-    return data.filter((select) => select.type.includes(selectByType));
-  };
-
-
-
+  if (orderBy === 'order-za') {
+    data.sort(function(a, b) {
+      var textA = a.name.toLowerCase();
+      var textB = b.name.toLowerCase();
+      return (textA > textB) ? -1 : (textA < textB) ? 1 : 0;
+    })
+  }
+  return data;
+};
--- src/main.js
+++ src/main.js
@@ -1,11 +1,27 @@
-import data from './data/pokemon/pokemon.js';
-const pokemons = data.pokemon;
+import data from './pokemon.js';
+import {selectType, searchByName, orderBy} from './data.js'
 
+const pokemons = data.pokemon;
 const cardPokemon = document.getElementById("card");
 
-function cardsPokemon() {
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+function cardsPokemon(pokemonArray) {
     let cards = "";
-    pokemons.forEach(pokemonAtual => {
+    pokemonArray.forEach(pokemonAtual => {
         const types = pokemonAtual.type;
         cards +=
             `<li class="card ${types[0]}">
@@ -14,18 +30,53 @@ function cardsPokemon() {
             <p class="card-subtitle" id="card-subtitle">${pokemonAtual.size.height} | ${pokemonAtual.size.weight} </p>
             <p class="card-subtitle2" id="card-subtitle2">${types.join(' | ')} </p>
             <p class="card-subtitle" id="card-subtitle">${pokemonAtual.generation.name} </p>
-        
             </li>`;
 
     });
     cardPokemon.innerHTML = cards;
 }
-cardsPokemon();
 
+cardsPokemon(pokemons);
 
-const filterSelectType = document.querySelector("filter-type")
-filterSelectType.addEventListener("chenge", () => {
-    const filterType = filterSelectType.valeu;
-    const displayType = selectType (filterType, data.pokemon);
-    cardsPokemon (displayType);
+// Filtros pro select (pré-prontos)
+
+const filterSelectType = document.querySelector("#filter-type");
+
+filterSelectType.addEventListener("change", (event) => {
+    const filterType = filterSelectType.value;
+    const arrayFiltered = selectType(filterType, data.pokemon);
+    cardsPokemon(arrayFiltered);
 })
+
+// Filtro de ordem select
+
+const filterSelectOrder = document.querySelector("#order-search");
+
+filterSelectOrder.addEventListener("change", (event) => {
+    const orderType = event.target.value;
+    const arrayOrdered = orderBy(orderType, data.pokemon);
+    cardsPokemon(arrayOrdered);
+})
+
+// Filtro por input (texto) de nome
+
+const filterInputType = document.querySelector("#search-input");
+
+filterInputType.addEventListener("change", (event) => {
+    const filterName = filterInputType.value;
+    const arrayFiltered = searchByName(filterName, data.pokemon);
+    cardsPokemon(arrayFiltered);
+})
+
+filterInputType.addEventListener("keyup", (event) => {
+
+    if (event.keyCode === 13) {
+        event.preventDefault();
+    }
+
+    const filterName = event.target.value;
+    const arrayFiltered = searchByName(filterName, data.pokemon);
+    cardsPokemon(arrayFiltered);
+
+})
+
