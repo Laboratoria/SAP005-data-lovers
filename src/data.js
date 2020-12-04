8d60ea32416fbe667e509a49ce69671ba0946aac
RoSapia <roberta.sapia@hotmail.com>
2020-12-04 20:05:52 -0300

    Iniciando testes

--- src/data.js
+++ src/data.js
@@ -1,28 +1,25 @@
-/*filterData(data, condition): esta função receberia os dados e nos retornaria os que cumprem com a condição.
-sortData(data, sortBy, sortOrder): esta função recebe três parâmetros. O primeiro, data, nos entrega os dados. O segundo, sortBy, diz respeito a qual das informações quer usar para ordenar. O terceiro, sortOrder, indica se quer ordenar de maneira crescente ou decrescente.
-computeStats(data): essa função nos permite fazer cálculos estatísticos básicos para serem exibidos de acordo com o que os dados permitem.
-*/
-
 const pokemon = {
   pesquisaPokemon(data, nomePokemon) {
     const listaPokemon = data.pokemon
-    let busca = listaPokemon.filter(p => p.name == nomePokemon)
-    return (busca)
+    const tipoParametro = typeof nomePokemon
+    if (typeof  tipoParametro === 'number'|| typeof  tipoParametro === 'undefined') {
+      throw new TypeError('Coloque o nome do Pokémon')
+    } else if (typeof nomePokemon === 'string') {
+      let busca = listaPokemon.filter(p => p.name == nomePokemon)
+      return (busca)
+    }
   },
   filtrarLendarios(data) {
     const listaPokemon = data.pokemon
     const lendarios = listaPokemon.filter(p => p['pokemon-rarity'] === 'legendary')
-    console.log(lendarios)
-    return (lendarios) 
+    return (lendarios)
   },
-  calculoAgregado(data, lendarios){
-    const quantLendarios = lendarios.length
+  calculoAgregado(data, pesquisado) {
+    const quantPesquisado = pesquisado.length
     const listaPokemon = data.pokemon
     const quantPokemons = listaPokemon.length
-    const porcLendarios = (quantLendarios*100)/quantPokemons
-    console.log(porcLendarios)
-    
-    return (porcLendarios.toFixed(1))
+    const porcPesquisado = (quantPesquisado * 100) / quantPokemons
+    return (porcPesquisado.toFixed(1))
   },
   ordenarPokemon(pokemons, valorOpcao) {
     if (valorOpcao == 'az') {
@@ -40,3 +37,6 @@ const pokemon = {
 }
 
 export default pokemon
+
+
+
