a645f3c626b0ef6b255521e3ff41bc98a9920aad
Priscila Silveira <priscilassilveira97@gmail.com>
2020-12-05 17:07:07 -0300

    readme

--- src/data.js
+++ src/data.js
@@ -1,7 +1,8 @@
-export function getName(allData, nome) {
-    nome = nome[0].toUpperCase() + nome.slice(1).toLowerCase();
-   return allData.filter(personagem => personagem.name.startsWith(nome))
+export function getName(allData, name) {
+    let filteredName = name[0].toUpperCase() + name.slice(1).toLowerCase();
+   return allData.filter(personage => personage.name.startsWith(filteredName))
 }
 
-   
+export const sortAZ =(allData) => allData.sort((a, b) => (a.name < b.name) ? - 1 : 1)
 
+export const sortZA =(allData) => allData.sort((a, b) => (a.name > b.name) ? - 1 : 1)
--- src/main.js
+++ src/main.js
@@ -1,79 +1,52 @@
 import rickAndMorty from './data/rickandmorty/rickandmorty.js';
-import { getName} from './data.js';
+import { getName, sortAZ, sortZA } from './data.js';
+
 const allData = rickAndMorty.results;
 
-//função filtrar
-let arrayFiltrada = allData;
+let filter = allData;
 
-
-
-
-
-
-
-document.getElementById("btn").addEventListener("click", (event)=> { 
+document.getElementById("btn").addEventListener("click", (event) => {
     event.preventDefault()
-    let nome = document.getElementById("texto").value;
-    const result = getName( allData, nome)
-    const secao = document.getElementById("frontCard")
-    secao.innerHTML = templateCard(result);
-})
-
-
-
-//função cards 
+    let name = document.getElementById("texto").value;
+    const result = getName(allData, name)
+    const section = document.getElementById("frontCard")
+    section.innerHTML = templateCard(result);
+});
 
 function templateCard(allData) {
-
-
-    let data = allData.map(personagem => `<div class="frontContainer"><img src=${personagem.image} alt="Avatar" class="img-card" /> 
-<br> <b class="name">${personagem.name}  
+    let data = allData.map(personage => `<div class="frontContainer"><img src=${personage.image} alt="Avatar" class="img-card" /> 
+<br> <b class="name">${personage.name}  
 </b></div>`)
-    // document.getElementById("frontCard").innerHTML = data;
 
-
-    let bData = allData.map(personagem => `<div class="backCard">  <h4><p> Name:  ${personagem.name} <br> Genus: 
-   ${personagem.gender} <br> Status: ${personagem.status} <br> Species: ${personagem.species} <br> 
-   Origin:${personagem.origin.name}<br> Location:${personagem.location.name}</h4></p></div>`)
-    document.getElementById("backCard").innerHTML = bData;
-
-
+    let backData = allData.map(personage => `<div class="backCard">  <h4><p> Name:  ${personage.name} <br> Genus: 
+   ${personage.gender} <br> Status: ${personage.status} <br> Species: ${personage.species} <br> 
+   Origin:${personage.origin.name}<br> Location:${personage.location.name}</h4></p></div>`)
+    document.getElementById("backCard").innerHTML = backData;
 
     return data
 }
-const secao = document.getElementById("frontCard")
-secao.innerHTML = templateCard(arrayFiltrada);
+const mappedCards = document.getElementById("frontCard")
+mappedCards.innerHTML = templateCard(filter);
 
-
-
-
-//função de ordenar
-
-document.getElementById("oBtn").addEventListener("click", ordC)
-function ordC(event) {
+document.getElementById("oBtn").addEventListener("click", orderAZ)
+function orderAZ(event) {
     event.preventDefault()
-    const ordenaAZ = allData.sort((a, b) => (a.name < b.name) ? - 1 : 1)
-    secao.innerHTML = templateCard(ordenaAZ);
-
+    const callOrderAZ = sortAZ(allData);
+    mappedCards.innerHTML = templateCard(callOrderAZ);
 }
 
-
-document.getElementById("aBtn").addEventListener("click", ordD)
-function ordD(event) {
+document.getElementById("aBtn").addEventListener("click", orderZA)
+function orderZA(event) {
     event.preventDefault()
-    const ordenaZA = allData.sort((a, b) => (a.name > b.name) ? - 1 : 1)
-    secao.innerHTML = templateCard(ordenaZA);
-
+    const callOrderZA = sortZA(allData)
+    mappedCards.innerHTML = templateCard(callOrderZA);
 }
-
-//calculo
 
 const statusData = allData.map(i => i.status)
 
 let alive = 0
 let dead = 0
 let unknown = 0
-
 
 for (let i = 0; i < statusData.length; i++) {
 
@@ -83,41 +56,34 @@ for (let i = 0; i < statusData.length; i++) {
     else if (statusData[i] === 'Dead') {
         dead += 1
     }
-    else  {
+    else {
         unknown += 1
     }
 }
-const total = alive + dead + unknown;
+const result = alive + dead + unknown;
 document.getElementById('alive').innerHTML = alive
 document.getElementById('dead').innerHTML = dead
 document.getElementById('unknown').innerHTML = unknown
-document.getElementById('total').innerHTML = total
+document.getElementById('accountResult').innerHTML = result
 
 
-
-//grafico
-
-google.charts.load('current', {'packages':['corechart']});
+google.charts.load('current', { 'packages': ['corechart'] });
 google.charts.setOnLoadCallback(drawChart);
 
-
 function drawChart() {
-  let graphic = google.visualization.arrayToDataTable([
-  ['STATUS', 'QUANTIDADE'],
-  ['vivo', alive],
-  ['morto', dead],
-   ['desconhecido', unknown]
-  
-]);
+    let graphic = google.visualization.arrayToDataTable([
+        ['STATUS', 'QUANTIDADE'],
+        ['vivo', alive],
+        ['morto', dead],
+        ['desconhecido', unknown]
+    ]);
 
-  let options = {'title':'STATUS', 'width':500, 'height':400 ,
-colors:['#BA55D3','#008080','#7B68EE']
-};
+    let options = {
+        'title': 'STATUS', 'width': 500, 'height': 400,
+        colors: ['#BA55D3', '#008080', '#7B68EE']
+    };
 
-
-
-  let chart = new google.visualization.PieChart(document.getElementById('piechart'));
-  chart.draw(graphic, options);
+    let chart = new google.visualization.PieChart(document.getElementById('piechart'));
+    chart.draw(graphic, options);
 }
-
 
