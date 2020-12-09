a0e6fa29b06694465091854720a1a0cb65ded0aa
cbalieiro <bmoliveira.camila@gmail.com>
2020-12-09 14:46:29 -0300

    JS/refatorado let para const, estruturas de array function no data e melhorias no teste

--- src/data.js
+++ src/data.js
@@ -1,23 +1,7 @@
-export const searchFind = (array, valueTxt) => {
-  return array.find(item => item.num === valueTxt.toLowerCase())
-}
+export const searchFind = (array, valueTxt) => array.find(item => item.num === valueTxt.toLowerCase())
 
-export const crescent = (array) => {
-  return array.sort(function (a, b) { return a.name > b.name ? 1 : -1; })
-}
+export const crescent = (array) => array.sort(function (a, b) { return a.name > b.name ? 1 : -1; })
 
-export const decrescent = (array) => {
-  return array.sort(function (a, b) { return a.name < b.name ? 1 : -1; })
-}
+export const decrescent = (array) => array.sort(function (a, b) { return a.name < b.name ? 1 : -1; })
 
-export const searchFilter = (array, valueTxt) => {
-  return array.filter((item) => item.type.includes(valueTxt))
-}
-
-// export const example = () => {
-//   return 'example';
-// };
-
-// export const anotherExample = () => {
-//   return 'OMG';
-// };
+export const searchFilter = (array, valueTxt) => array.filter((item) => item.type.includes(valueTxt))
--- src/main.js
+++ src/main.js
@@ -1,146 +1,148 @@
-import { crescent, decrescent, searchFilter, searchFind } from './data.js';
+import {crescent,decrescent,searchFilter,searchFind} from './data.js';
 import data from './data/pokemon/pokemon.js';
 const copyDB = data.pokemon;
 
-
-// Limpa a DIV que irá receber todos os CARDS (Todos, Filtrados ou Ordenados) 
 function clearCards() {
-    document.getElementById("item-all-cards").innerHTML = ""
+  document.getElementById("item-all-cards").innerHTML = ""
 }
 
-/*                            Funções para Templates de Cards                            */
 function templateOneCard(array) {
-    let pokecard = "";
-    pokecard += 
-            `<div id="item-card1">
+  let pokecard = "";
+  pokecard +=
+    `
+            <div id="item-card1">
                 <img src="${array.img}">
-                <h1>${array.name.toUpperCase()}<h1>
+                <h2>${array.name.toUpperCase()}<h2>
                 <p>${array.num}</p> 
                 <p>${array.about}</p> 
                 <p> <strong>Tipo:</strong> ${array.type.join(' / ')}</p>
                 <p> <strong>Resistente:</strong> ${array.resistant.join(' / ')}</p>
-            </div>`
-            
-
-    return document.getElementById("item-card-one-front").innerHTML = pokecard
+            </div>
+        `
+  return document.getElementById("item-card-one-front").innerHTML = pokecard
 }
 
 function templateOneCardBack(array) {
-    let pokecardback = "";
-    pokecardback += 
-            `<div id="item-card-back">
+  let pokecardback = "";
+  pokecardback +=
+    `
+            <div id="item-card-back">
                 <p><strong> Height:</strong> ${array.size.height}</p>
                 <p><strong> Weight:</strong> ${array.size.weight}</p>
                 <p> <strong>Generation:</strong> ${array.generation.num}</p>
                 <p> <strong>Weaknesses:</strong> ${array.weaknesses.join(' / ')}</p>
                 <p><strong> Candy- Evolution:</strong> ${array.evolution.candy}</p>  
                 <p> <strong>Egg:</strong> ${array.egg}</p> 
-            </div>`
-            
-
-    return document.getElementById("item-card-one-back").innerHTML = pokecardback
+            </div>
+        `
+  return document.getElementById("item-card-one-back").innerHTML = pokecardback
 }
-
-
 
 function templateAllCards(array) {
-    let pokecards = "";
-    for (let indice of array) {
-        pokecards += `<div class="mostrar">
-<img src="${indice.img}">
-<h1>${indice.name.toUpperCase()}<h1>
-<p>${indice.num}</p> 
-<p>${indice.about}</p> 
-<p> <strong>Tipo:</strong> ${indice.type.join(' / ')}</p>
-<p> <strong>Resistente:</strong> ${indice.resistant.join(' / ')}</p></div>`
-    }
-    return document.getElementById("item-all-cards").innerHTML = pokecards
+  let pokecards = "";
+  for (let indice of array) {
+    pokecards +=
+      `
+            <div class="mostrar">
+                <img src="${indice.img}">
+                <h2>${indice.name.toUpperCase()}<h2>
+                <p>${indice.num}</p> 
+                <p>${indice.about}</p> 
+                <p> <strong>Tipo:</strong> ${indice.type.join(' / ')}</p>
+                <p> <strong>Resistente:</strong> ${indice.resistant.join(' / ')}</p>
+            </div>
+        `
+  }
+  return document.getElementById("item-all-cards").innerHTML = pokecards
 }
-// Qual a diferença dos dois templates? 1º é para resultado individual e 2º é para resultado que demanda passar pela varredura em loop.
 
-/*                            Todas as funções ---> Na sequência > CardUnitário> TodosOsCards> OrdenaCrescent > OrdenaDecrescente > FiltraporTipo                          */
 function cardOne() {
-    document.getElementById("item-card-one-front").innerHTML = "";
-    let txtSearch = document.getElementById('txtBusca').value;
-    const resultSearch = searchFind(copyDB, txtSearch);
-    return templateOneCard(resultSearch)
+  document.getElementById("item-card-one-front").innerHTML = "";
+  const txtSearch = document.getElementById('txtBusca').value;
+  let txtSearchTransf = txtSearch
+    if (txtSearch.length == 2) {
+        txtSearchTransf = '0' + txtSearch;
+    }
+    if (txtSearch.length == 1) {
+        txtSearchTransf = '00' + txtSearch;
+    }
+  const resultSearch = searchFind(copyDB, txtSearchTransf);
+    if (resultSearch == undefined) {
+        document.getElementById("item-card-one-front").innerHTML = `<h3>Pokemon não encontrado</h3>`
+    }
+  return templateOneCard(resultSearch)
 }
 
 function cardOneBack() {
-    document.getElementById("item-card-one-back").innerHTML = "";
-    let txtSearch = document.getElementById('txtBusca').value;
-    const resultSearch = searchFind(copyDB, txtSearch);
-    return templateOneCardBack(resultSearch)
+  document.getElementById("item-card-one-back").innerHTML = "";
+  const txtSearch = document.getElementById('txtBusca').value;
+  let txtSearchTransf = txtSearch
+    if (txtSearch.length == 2) {
+        txtSearchTransf = '0' + txtSearch;
+    }
+    if (txtSearch.length == 1) {
+        txtSearchTransf = '00' + txtSearch;
+     }
+  const resultSearch = searchFind(copyDB, txtSearchTransf);
+  return templateOneCardBack(resultSearch)
 }
 
-
-
-
-// Botão e Event para enter localizados na section class="middle-bg" do html.
 document.getElementById('btn-search-pokemon').addEventListener("click", cardOne);
 
 let input = document.getElementById("txtBusca");
 input.addEventListener("keyup", function (event) {
-    if (event.key === 'Enter') {
-        event.preventDefault();
-        cardOne()
-    }
+  if (event.key === 'Enter') {
+    event.preventDefault();
+    cardOne()
+  }
 })
 
 document.getElementById('btn-search-pokemon').addEventListener("click", cardOneBack);
 let input2 = document.getElementById("txtBusca");
 input2.addEventListener("keyup", function (event) {
-    if (event.key === 'Enter') {
-        event.preventDefault();
-        cardOneBack()
-    }
+  if (event.key === 'Enter') {
+    event.preventDefault();
+    cardOneBack()
+  }
 })
 
-
-
-//Responsável por carregar em Cards todo o Objeto Data / Array Pokemon.
 function cardAll() {
-    clearCards();
-    return templateAllCards(copyDB)
+  clearCards();
+  return templateAllCards(copyDB)
 }
 
 document.getElementById('btn-generation-pokemon').addEventListener("click", cardAll);
 
-//Responsável por Ordenar Crescente:
 function sortByABC() {
-    clearCards();
-    const callSort = crescent(copyDB);
-    return templateAllCards(callSort)
+  clearCards();
+  const callSort = crescent(copyDB);
+  return templateAllCards(callSort)
 }
 
 document.getElementById('btn-cresc-pokemon').addEventListener("click", sortByABC)
 
-//Responsável por Ordenar Decrescente:
 function sortByZYX() {
-    clearCards();
-    const callSort = decrescent(copyDB);
-    return templateAllCards(callSort)
+  clearCards();
+  const callSort = decrescent(copyDB);
+  return templateAllCards(callSort)
 }
 
 document.getElementById('btn-decresc-pokemon').addEventListener("click", sortByZYX);
 
-//Responsável por Filtrar:
 function filterByType() {
-    clearCards();
-    let filterTypeSelect = document.getElementById("filter-type-select").value;
-    const callFilter = searchFilter(copyDB, filterTypeSelect);
-    return ([templateAllCards(callFilter)],[aggregateCalculation(callFilter)])
+  clearCards();
+  const filterTypeSelect = document.getElementById("filter-type-select").value;
+  const callFilter = searchFilter(copyDB, filterTypeSelect);
+  return ([templateAllCards(callFilter)], [aggregateCalculation(callFilter)])
 }
 
 document.getElementById('btn-filter-type').addEventListener("click", filterByType)
 
-//Responsável pelo Calculo Agregado:
-function aggregateCalculation (array) {
-let filterTypeSelect = document.getElementById("filter-type-select").value;
-const sizeArray = copyDB.length
-const sizeFilter = array.length
-const result = ((sizeFilter/sizeArray)*100).toFixed(0)+"%";
-const aggregateCal = `<h3>O tipo ${filterTypeSelect} representa ${result} do total de Pokémons</h3>`
-return document.getElementById("item-aggregate-calc").innerHTML = aggregateCal;
-
+function aggregateCalculation(array) {
+  const filterTypeSelect = document.getElementById("filter-type-select").value;
+  const sizeArray = copyDB.length
+  const sizeFilter = array.length
+  const result = ((sizeFilter / sizeArray) * 100).toFixed(0) + "%";
+  const aggregateCal = `<h3>O tipo ${filterTypeSelect} representa ${result} do total de Pokémons</h3>`
+  return document.getElementById("item-aggregate-calc").innerHTML = aggregateCal;
 }
