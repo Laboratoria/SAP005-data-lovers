430fae714ac62a2e4a9c5502c3c1b4cf9ca6bb9e
Juliana <juliana.souzas@yahoo.com.br>
2020-12-09 13:20:09 -0300

    refatoracao

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { filterGenderSelected, filterStatusSelected, filterSpeciesSelected, sortOrder, calcGender, calcStatus, calcSpecies } from './data.js';
 import data from './data/rickandmorty/rickandmorty.js';
 const dataBase = data.results;
+const filterSelect = document.querySelector('.filter-gender');
+const filterSelectStatus = document.querySelector('.filter-status');
+const filterSelectSpecies = document.querySelector('.filter-species');
+const filterSelectOrder = document.querySelector('.filter-order');
+
+
 function printCharacters(dados) {
     let cards = "";
     for (let item of dados) {
@@ -10,9 +16,9 @@ function printCharacters(dados) {
       <div class="frente">
         <p class="card-name">${item.name.toUpperCase()}</p>
         <img src="${item.image}" class="card-img">
-      <p class="card-tras">${item.status.toUpperCase()}</p>
-      <p class="card-tras">${item.species.toUpperCase()}</p>
-      <p class="card-tras">${item.gender.toUpperCase()}</p>
+      <p class="card-tras">Status: ${item.status.toUpperCase()}</p>
+      <p class="card-tras">Espécie: ${item.species.toUpperCase()}</p>
+      <p class="card-tras">Gênero: ${item.gender.toUpperCase()}</p>
       <p class="card-tras">${item.location.name.toUpperCase()}</p>
     </div>
     </article>
@@ -20,8 +26,12 @@ function printCharacters(dados) {
     }
     document.getElementById("hero").innerHTML = cards
 } printCharacters(dataBase);
-const filterSelect = document.querySelector('.filter-gender');
+
 filterSelect.addEventListener('change', filterGender)
+filterSelectStatus.addEventListener('change', filterStatus)
+filterSelectSpecies.addEventListener('change', filterSpecies)
+filterSelectOrder.addEventListener('change', filterOrder)
+
 function filterGender() {
     const valueGenderSelected = filterSelect.value
     const selectedGender = filterGenderSelected(dataBase, valueGenderSelected)
@@ -29,8 +39,7 @@ function filterGender() {
     let resultGender = calcGender(dataBase, selectedGender)
     document.getElementById("calculation").innerHTML = "Existem " + selectedGender.length + " personagens deste gênero e representa " + resultGender + "% do total de personagens"
 }
-const filterSelectStatus = document.querySelector('.filter-status');
-filterSelectStatus.addEventListener('change', filterStatus)
+
 function filterStatus() {
     const valueStatusSelected = filterSelectStatus.value
     const selectedStatus = filterStatusSelected(dataBase, valueStatusSelected)
@@ -38,8 +47,7 @@ function filterStatus() {
     let resultStatus = calcStatus(dataBase, selectedStatus)
     document.getElementById("calculation").innerHTML = "Existem " + selectedStatus.length + " personagens neste status e representa " + resultStatus + "% do total de personagens"
 }
-const filterSelectSpecies = document.querySelector('.filter-species');
-filterSelectSpecies.addEventListener('change', filterSpecies)
+
 function filterSpecies() {
     const valueSpeciesSelected = filterSelectSpecies.value
     const selectedSpecies = filterSpeciesSelected(dataBase, valueSpeciesSelected)
@@ -47,11 +55,11 @@ function filterSpecies() {
     let resultSpecies = calcSpecies(dataBase, selectedSpecies)
     document.getElementById("calculation").innerHTML = "Existem " + selectedSpecies.length + " personagens desta espécie e representa " + resultSpecies + "% do total de personagens"
 }
-const filterSelectOrder = document.querySelector('.filter-order');
-filterSelectOrder.addEventListener('change', filterOrder)
+
 function filterOrder() {
     document.getElementById("calculation").innerHTML = ""
     const valueOrderSelected = filterSelectOrder.value
     const selectedOrder = sortOrder(dataBase, valueOrderSelected)
     printCharacters(selectedOrder)
 }
+
